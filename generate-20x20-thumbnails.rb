34e3e7749b112499a4037bd6bfc25e07a713800a
Roland Tanglao <roland@rolandtanglao.com>
2024-05-14 23:21:50 -0700

    change it to handle non jpegs

--- generate-20x20-thumbnails.rb
+++ generate-20x20-thumbnails.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'amazing_print'
 require 'logger'
 require 'mini_magick'
+require 'rake'
 logger = Logger.new($stderr)
 logger.level = Logger::DEBUG
 
@@ -11,10 +12,10 @@ ARGF.each_line.with_index do |photo_filename, i|
   photo_without_dot_slash = photo_filename.chomp[2..]
   image = MiniMagick::Image.open(photo_without_dot_slash)
   image.resize '20x20'
-  image.format 'jpg'
+  image.format 'png'
   logger.debug "photo_without_dot_slash:#{photo_without_dot_slash}"
   dirname = File.dirname(photo_without_dot_slash)
-  filename = "THUMBS/#{format('%5.5d', (i + 1))}-#{dirname}-#{File.basename(photo_without_dot_slash)}"
+  filename = "THUMBS/#{format('%5.5d', (i + 1))}-#{dirname}-#{File.basename(photo_without_dot_slash)}".ext('png')
   logger.debug "filename:#{filename}"
   image.write filename
 end
